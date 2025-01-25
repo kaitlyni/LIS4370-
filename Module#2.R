565d5527f08414d588017036ea3f7f0cc6503b71
kaitlyni <kaitlyniliev@usf.edu>
2025-01-24 20:39:33 -0500

    Create Module#2.R

---
+++ Module#2.R
@@ -0,0 +1,8 @@
+assignment2 <- c(16, 18, 14, 22, 27, 19, 17, 17, 22, 20, 22)
+
+myMean <- function(assignment2){
+  return(sum(assignment2)/length(assignment2))
+}
+
+result <- myMean(assignment2)
+result
